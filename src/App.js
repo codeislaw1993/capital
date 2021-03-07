4ccd97f642c356c58902741c36eb2dce6bdef764
jaycheungchunman <cheung1993@gmail.com>
2021-03-07 16:06:28 +0800

    remove warning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
-import { purple } from '@material-ui/core/colors';
 import Grid from '@material-ui/core/Grid';
 import React, { useState, useEffect } from 'react';
 import PancakeFactory from './abi/PancakeFactory.json';
