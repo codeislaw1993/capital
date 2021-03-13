161ffb85ce791933f7f8354482cd35ad6d3a0f9f
jaycheungchunman <cheung1993@gmail.com>
2021-03-13 17:52:52 +0800

    add farm, quote price

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Grid from '@material-ui/core/Grid';
 import React, { useState, useEffect } from 'react';
 import PancakeFactory from './abi/PancakeFactory.json';
 import PancakeRouter from './abi/PancakeRouter.json';
 import Web3 from 'web3';
+import { Button, ButtonGroup, Container, Row, Col, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MY_CONTRACT = process.env.REACT_APP_MY_CONTRACT
 const MY_ROUTER_CONTRACT=process.env.REACT_APP_MY_ROUTER_CONTRACT
 const MY_HONG_CONTRACT=process.env.REACT_APP_MY_HONG_CONTRACT
 const MY_BUSD_CONTRACT=process.env.REACT_APP_MY_BUSD_CONTRACT
 const MY_LP_CONTRACT=process.env.REACT_APP_MY_LP_CONTRACT
+const MY_MASTERCHEF_CONTRACT=process.env.REACT_APP_MY_MASTERCEHF_CONTRACT
 
 function App() {
 
@@ -22,7 +22,10 @@ function App() {
   const [myHongContract, setMyHongContract] = useState();
   const [myBUSDContract, setMyBUSDContract] = useState();
   const [myLPContract, setMyLPContract] = useState();
+  const [myMasterChefContract, setMyMasterChefContract] = useState();
   const [currentNetworkID,setcurrentNetworkID] = useState();
+  const [amountOut,setAmountOut] = useState();
+  const [amountIn,setAmountIn] = useState();
 
   const ethEnabled = async() => {
     if (window.web3) {
@@ -32,24 +35,47 @@ function App() {
       const accounts = await web3js.eth.getAccounts(function (result) {
         console.log("account error:", result);
       });
+
       const networkID = await web3js.eth.net.getId();
       if (networkID) {
         setcurrentNetworkID(networkID)
       }
 
-      console.log(accounts);
       setMyAccount(accounts[0])
-      console.log("Account connected?");
-      console.log(myAccount);
-      console.log(currentNetworkID);
 
-      const data = require('./abi/Hong.json');
+      const dataHong = require('./abi/Hong.json');
+      const dataLpToken = require('./abi/lpToken.json');
+      const dataMasterChef = require('./abi/MasterChef.json');
 
       setMyContract(await new window.web3.eth.Contract(PancakeFactory.abi, MY_CONTRACT));
       setMyRouterContract(await new window.web3.eth.Contract(PancakeRouter.abi, MY_ROUTER_CONTRACT));
-      setMyHongContract(await new window.web3.eth.Contract(data, MY_HONG_CONTRACT));
-      setMyBUSDContract(await new window.web3.eth.Contract(data, MY_BUSD_CONTRACT));
-      setMyLPContract(await new window.web3.eth.Contract(data, MY_LP_CONTRACT));
+      setMyHongContract(await new window.web3.eth.Contract(dataHong, MY_HONG_CONTRACT));
+      setMyBUSDContract(await new window.web3.eth.Contract(dataHong, MY_BUSD_CONTRACT));
+      setMyLPContract(await new window.web3.eth.Contract(dataLpToken, MY_LP_CONTRACT));
+      setMyMasterChefContract(await new window.web3.eth.Contract(dataMasterChef, MY_MASTERCHEF_CONTRACT));
+    }
+  }
+
+  const refreshPrice = () => {
+
+    if (myRouterContract) {
+      let argsGetAmountsOut: Array<string | string[] | number> = [
+        window.web3.utils.toBN(100000000000000000).toString(),
+        [MY_HONG_CONTRACT, MY_BUSD_CONTRACT]
+      ]
+
+      let argsGetAmountsIn: Array<string | string[] | number> = [
+        window.web3.utils.toBN(100000000000000000).toString(),
+        [MY_BUSD_CONTRACT, MY_HONG_CONTRACT]
+      ]
+
+      myRouterContract.methods.getAmountsOut(...argsGetAmountsOut).call({from: myAccount}, function (result, error) {
+        setAmountOut(error[1] / error[0]);
+      });
+
+      myRouterContract.methods.getAmountsOut(...argsGetAmountsIn).call({from: myAccount}, function (result, error) {
+        setAmountIn(error[1] / error[0]);
+      });
     }
   }
 
@@ -184,38 +210,142 @@ function App() {
     return false;
   }
 
+  const stake = () => {
+    if (myMasterChefContract) {
+      let args: Array<string | string[] | number> = [
+        window.web3.utils.toBN(1).toString(),
+        window.web3.utils.toBN(100000000000000000).toString()
+      ]
+
+      let approveArgs: Array<string | string[] | number> = [
+        MY_MASTERCHEF_CONTRACT,
+        window.web3.utils.toBN(1000000000000000000).toString()
+      ]
+
+      myLPContract.methods.approve(...approveArgs).send({from: myAccount}, function (result, error) {
+        console.log("myLPContract approve");
+        console.log(result);
+        console.log(error);
+
+        myMasterChefContract.methods.deposit(...args).send({from: myAccount}, function(result, error) {
+          console.log("myMasterChefContract deposit");
+          console.log(result);
+          console.log(error);
+        });
+      });
+    }
+    return false;
+  }
+
+  const unstake = () => {
+    if (myMasterChefContract) {
+      let args: Array<string | string[] | number> = [
+        window.web3.utils.toBN(1).toString(),
+        window.web3.utils.toBN(100000000000000000).toString()
+      ]
+
+      myMasterChefContract.methods.withdraw(...args).send({from: myAccount}, function(result, error) {
+        console.log("myMasterChefContract withdraw");
+        console.log(result);
+        console.log(error);
+      });
+    }
+    return false;
+  }
+
+  const pendingReward = () => {
+    if (myMasterChefContract) {
+      let args: Array<string | string[] | number> = [
+        window.web3.utils.toBN(1).toString(),
+        myAccount
+      ]
+
+      myMasterChefContract.methods.pendingCake(...args).call({from: myAccount}, function(result, error) {
+        console.log("myMasterChefContract pendingCake");
+        console.log(result);
+        console.log(error);
+      });
+    }
+    return false;
+  }
+
+  const poolInfo = () => {
+    if (myMasterChefContract) {
+      let args: Array<string | string[] | number> = [
+        window.web3.utils.toBN(1).toString()
+      ]
+
+      myMasterChefContract.methods.poolInfo(...args).call({}, function(result, error) {
+        console.log("myMasterChefContract poolInfo");
+        console.log(result);
+        console.log(error);
+      });
+    }
+    return false;
+  }
+
+  const userInfo = () => {
+    if (myMasterChefContract) {
+      let args: Array<string | string[] | number> = [
+        window.web3.utils.toBN(1).toString(),
+        myAccount
+      ]
+
+      myMasterChefContract.methods.userInfo(...args).call({}, function(result, error) {
+        console.log("myMasterChefContract userInfo");
+        console.log(result);
+        console.log(error);
+      });
+    }
+    return false;
+  }
+
   useEffect(() => {
     document.title = "Capital"
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Capital
-        </p>
-        <Grid container>
-          <Grid item xs={12}>
-            <ButtonGroup color="primary" aria-label="outlined primary button group">
-              <Button onClick={ethEnabled}>Connect Wallet</Button>
-            </ButtonGroup>
-          </Grid>
-          <Grid item xs={12}>
-            <ButtonGroup orientation="vertical" variant="text" color="primary" aria-label="outlined primary button group">
-              <Button color="primary" onClick={addLiquidityFunction}>Add Liquidity</Button>
-              <Button color="secondary" onClick={removeLiquidityFunction}>Remove Liquidity</Button>
-            </ButtonGroup>
-          </Grid>
-          <Grid item xs={12}>
-            <ButtonGroup orientation="vertical" variant="text" color="primary" aria-label="outlined primary button group">
-              <Button color="primary" onClick={swapBUSDToHONG}>From 0.1 BUSD to HONG </Button>
-              <Button color="secondary" onClick={swapHONGToBUSD}>From 0.1 HONG to BUSD </Button>
-            </ButtonGroup>
-          </Grid>
-        </Grid>
-      </header>
-    </div>
+        <div className="App">
+        <header className="App-header">
+          <Container>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>HONG/BUSD Swap</Card.Title>
+                    <Card.Subtitle>1 BUSD to {amountIn} HONG</Card.Subtitle>
+                    <Card.Subtitle>&nbsp;</Card.Subtitle>
+                    <Card.Subtitle>1 HONG to {amountOut} BUSD</Card.Subtitle>
+                    <ListGroup className="list-group-flush">
+                      <ListGroupItem><Button variant="light" onClick={ethEnabled}>Connect Wallet</Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={refreshPrice}>Refresh Price</Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={swapBUSDToHONG}>From 0.1 BUSD to HONG </Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={swapHONGToBUSD}>From 0.1 HONG to BUSD </Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={addLiquidityFunction}>Add 1:1 Liquidity</Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={removeLiquidityFunction}>Remove 1:1 Liquidity</Button></ListGroupItem>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>HONG/BUSD Liquidity Farm </Card.Title>
+                    <Card.Subtitle>Rewards: HONG</Card.Subtitle>
+                    <ListGroup className="list-group-flush">
+                      <ListGroupItem><Button variant="light" onClick={poolInfo}>Farm Info </Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={userInfo}>My Staking Info</Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={stake}>Stake 1 LP to Farm </Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={unstake}>Unstake 1 LP to Farm </Button></ListGroupItem>
+                      <ListGroupItem><Button variant="light" onClick={pendingReward}>Show pending rewards </Button></ListGroupItem>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </header>
+        </div>
   );
 }
 
